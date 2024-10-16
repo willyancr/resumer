9c65b06896327aa4850ce22bcb2fc28d747d5ce9
Willyan Costa <willyancr@gmail.com>
2024-10-16 18:10:25 -0300

    feat: Add fetch URL content API route and update MainContent component
    - Added a new API route to fetch URL content using Readability and Axios.
    - Updated the MainContent component to handle URL submission and display
      the fetched article content.
    - Implemented a function to remove images from HTML content for better
      optimization.
    - Updated global CSS to style scrollbars for better user experience.

---
+++ src/app/api/fetch-url-content/route.ts
@@ -0,0 +1,61 @@
+import { Readability } from "@mozilla/readability";
+import axios from "axios";
+import { JSDOM } from "jsdom";
+
+export async function POST(req: Request) {
+  try {
+    // Extrair a URL do corpo da requisição
+    const { url } = await req.json();
+    console.log("URL RECEBIDA", url);
+
+    // Validar a URL antes de prosseguir
+    try {
+      new URL(url);
+    } catch {
+      // Retornar erro se a URL for inválida
+      return new Response(JSON.stringify({ error: "URL inválida" }), {
+        status: 400,
+      });
+    }
+
+    // Fazer a requisição GET para a URL fornecida
+    const response = await axios.get(url);
+    console.log("Status da resposta:", response.status);
+
+    // Criar um DOM a partir do conteúdo HTML recebido
+    const dom = new JSDOM(response.data, { url });
+    // Inicializar o Readability com o documento do DOM
+    const reader = new Readability(dom.window.document);
+    // Extrair o conteúdo principal da página
+    const article = reader.parse();
+
+    // Verificar se o Readability conseguiu processar o conteúdo
+    if (!article) {
+      console.error("Erro: Readability não conseguiu processar o conteúdo");
+      return new Response(
+        JSON.stringify({ error: "Erro ao processar o conteúdo da página" }),
+        { status: 500 },
+      );
+    }
+
+    // Retornar o conteúdo processado
+    return new Response(
+      JSON.stringify({
+        title: article?.title,
+        content: article?.content,
+        excerpt: article?.excerpt,
+      }),
+      {
+        status: 200,
+        headers: { "Content-Type": "application/json" },
+      },
+    );
+  } catch (error) {
+    // Tratar erros gerais
+    console.error("Falha ao buscar conteúdo da URL", error);
+    return new Response(
+      JSON.stringify({ error: "Falha ao buscar o conteúdo da URL" }),
+      { status: 500 },
+    );
+  }
+}
--- src/app/components/main-content.tsx
+++ src/app/components/main-content.tsx
@@ -1,7 +1,64 @@
+"use client";
+import { useState } from "react";
+
+type Article = {
+  title: string;
+  content: string;
+  excerpt: string;
+};
+
 export default function MainContent() {
+  const [url, setUrl] = useState<string>("");
+  const [article, setArticle] = useState<Article>({
+    title: "",
+    content: "",
+    excerpt: "",
+  });
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!url) {
+      console.error("URL inválida");
+      return;
+    }
+
+    fetch("/api/fetch-url-content", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        url,
+      }),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setArticle(data);
+      })
+      .catch((error) => {
+        console.error("Falha ao buscar conteúdo da URL", error);
+      });
+  };
+
+  // Função para remover imagens de um HTML
+  const removeImagesHtml = (html: string) => {
+    // Cria um novo objeto DOMParser e analisa a string HTML
+    const doc = new DOMParser().parseFromString(html, "text/html");
+    const images = doc.querySelectorAll("img");
+
+    images.forEach((image) => image.remove());
+    // Retorna o HTML do corpo do documento sem as imagens
+    return doc.body.innerHTML;
+  };
+
+  const optimizeContent = removeImagesHtml(article.content);
+
   return (
     <div className="mx-auto flex w-full max-w-[1200px] flex-col items-center justify-between gap-12 px-4 py-8 md:px-12">
-      <div className="mx-auto w-[600px]">
+      <div className="mx-auto w-[700px]">
         <h1 className="text-center text-2xl font-bold text-zinc-300">
           Com o <span className="text-3xl text-zinc-50">Resumer</span>, você
           obtém resumos inteligentes e rápidos das matérias que desejar, usando
@@ -9,25 +66,36 @@ export default function MainContent() {
         </h1>
       </div>
       <div className="flex flex-col gap-12">
-        <div className="mx-auto flex w-[600px] items-center justify-between gap-2 rounded-xl border border-zinc-500 px-6 py-3">
+        <form
+          onSubmit={handleSubmit}
+          className="mx-auto flex w-[700px] items-center justify-between gap-2 rounded-xl border border-zinc-500 px-6 py-3"
+        >
           <input
             type="text"
             placeholder="Cole a URL"
-            className="w-[500px] rounded-md bg-zinc-800 px-4 py-3 font-semibold outline-0"
+            className="w-[600px] rounded-md bg-zinc-800 px-4 py-3 font-semibold outline-0"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
           />
-          <button className="rounded-md bg-zinc-200 px-4 py-3 text-lg font-semibold text-zinc-950 transition-colors hover:bg-zinc-300">
+          <button
+            type="submit"
+            className="rounded-md bg-zinc-200 px-4 py-3 text-lg font-semibold text-zinc-950 transition-colors hover:bg-zinc-300"
+          >
             Resumer
           </button>
-        </div>
+        </form>
         <div>
-          <div className="h-[400px] w-[600px] rounded-md bg-zinc-800 px-4 py-3 font-semibold outline-0">
-            <p className="text-lg text-zinc-300">
-              {" "}
-              Resumo gerado com o Resumer, um AI que gera resumos de matérias de
-              jornalísticas a partir de URLs. Simplifique sua leitura com
-              resumos rápidos, precisos e fáceis de entender.
-            </p>
-          </div>
+          {article && (
+            <div className="h-[400px] w-[700px] overflow-y-auto rounded-md bg-zinc-800 px-6 py-4 font-semibold outline-0">
+              <h1 className="text-lg text-zinc-300">{article.title}</h1>
+              <p className="mb-4 italic text-zinc-300">{article.excerpt}</p>
+              {/* O uso dangerouslySetInnerHTML é responsável por permitir que o conteúdo HTML seja renderizado corretamente. */}
+              <div
+                className="prose mb-4 text-zinc-300"
+                dangerouslySetInnerHTML={{ __html: optimizeContent }}
+              />
+            </div>
+          )}
         </div>
       </div>
     </div>
